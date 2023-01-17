887eee613f1822c1fa42e67f61313a4d8ea85127
Isadora Ávila <isadorgravila@gmail.com>
2023-01-17 12:31:29 -0300

    exercicio: ola spring

---
+++ src/main/java/com/olaspring/api/controller/OlaController.java
@@ -0,0 +1,15 @@
+package com.olaspring.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ola")
+public class OlaController {
+
+  @GetMapping
+  public String olaString() {
+    return "Olá Spring!";
+  }
+}
